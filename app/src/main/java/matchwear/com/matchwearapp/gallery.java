ff873df853efdbdef006aeb9015e6aa65af74524
ZP4 <jasonhuzheming9@gmail.com>
2016-10-31 09:45:12 -0400

    Created class

---
+++ app/src/main/java/matchwear/com/matchwearapp/gallery.java
@@ -0,0 +1,29 @@
+package matchwear.com.matchwearapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class gallery extends Fragment {
+
+    // Required empty public constructor
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+
+
+
+        return view;
+    }
+
+}
