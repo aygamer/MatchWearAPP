691b7345c60c6d977b3d5f07d9e88112a05a28f7
aygamer <yanganthony212@gmail.com>
2016-11-07 09:33:32 -0500

    Update LuetBox.java
    add button for popup

--- app/src/main/java/matchwear/com/matchwearapp/LuetBox.java
+++ app/src/main/java/matchwear/com/matchwearapp/LuetBox.java
@@ -3,9 +3,13 @@ package matchwear.com.matchwearapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 
 /**
@@ -18,12 +22,38 @@ public class LuetBox extends Fragment {
         // Required empty public constructor
     }
 
-
+    private Button LuetButton;
+    private String hi;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_luet_box, container, false);//when wrking with fragments create a view
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_luet_box, container, false);
+
+        final PopupWindow popup = new PopupWindow(getActivity());
+        Button LuetButton = (Button) view.findViewById(R.id.mbutton);//referencing from fragment_luet_box
+        final LinearLayout mainlayout = new LinearLayout(getActivity());
+        LuetButton.setOnClickListener(new View.OnClickListener() {
+            boolean clicked=true;
+            @Override
+            public void onClick(View v) {
+                if (clicked){
+                    clicked=false;
+                    popup.showAtLocation(mainlayout, Gravity.CENTER,20,20);
+                    popup.update(50,50,100,100);
+                }
+                else{
+                    clicked=true;
+                    popup.dismiss();
+                }
+
+
+            }
+        });
+
+
+        return view;
+
     }
 
 }
